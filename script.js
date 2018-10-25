551e39cc6adf957344abdaf8d34766c5311f7961
Ranchino Bahadin <ranjbahadin@live.se>
2018-10-26 00:00:18 +0200

    adding login function

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-user {
-    var userName = ranchino;
-    var password = admin;
+
+
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+    var userName = document.getElementsByClassName("userName").value;
+    var password = document.getElementsByClassName("password").value;
+    if ( userName == "admin" && password == "admin" ){
+        alert ("Du har loggat in!");
+        window.location = "index.html"; // Redirecting to other page.
+        return true;
+    }
+    else{
+    attempt --;// Decrementing by one.
+    alert("Du har "+attempt+" försök kvar;");
+    } if( attempt == 0){ // Disabling fields after 3 attempts.
+
+    alert = false, 
+    document.getElementsByClassName("userName").value = "";
+    document.getElementsByClassName("password").value= "";    
+    return;
+    }
 }
+
